637f3b623615c10d5c09114e4a72104436e3656b
LuizLima <luiz.c.brito1@gmail.com>
2021-08-18 18:04:25 -0300

    [Refactor] Refactoring validation package and Marca Entity + removing validation function on MarcaController + add sonarCloud on project (#1)
    * adjustments to postgres + sonarcloud
    
    * adjustment on controller validation handler + marcaCtrl and MarcaEntity
    
    * revert: revert entity name + adjust handler name
    
    * feat: add build.yml for sonacloud
    
    * Revert "feat: add build.yml for sonacloud"
    
    This reverts commit 2aef7b9727a5741ee5dc54cfe4a1164993fcf03e.

--- src/main/java/br/com/caelum/carangobom/validacao/ErroDeParametroOutputDto.java
+++ src/main/java/br/com/caelum/carangobom/validacao/ErroDeParametroOutputDto.java
@@ -1,23 +1,12 @@
 package br.com.caelum.carangobom.validacao;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class ErroDeParametroOutputDto {
 
     private String parametro;
     private String mensagem;
-
-    public String getParametro() {
-        return parametro;
-    }
-
-    public void setParametro(String parametro) {
-        this.parametro = parametro;
-    }
-
-    public String getMensagem() {
-        return mensagem;
-    }
-
-    public void setMensagem(String mensagem) {
-        this.mensagem = mensagem;
-    }
 }
