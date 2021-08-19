c9f6a8e7a9330f1770bbcc127d91958b116da6cb
Luiz Lima <luiz.c.brito1@gmail.com>
2021-08-19 18:23:23 -0300

    test: Add Marca dto's tests

--- src/main/java/br/com/caelum/carangobom/marca/MarcaForm.java
+++ src/main/java/br/com/caelum/carangobom/marca/MarcaForm.java
@@ -1,8 +1,14 @@
 package br.com.caelum.carangobom.marca;
 
-import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-@Data
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class MarcaForm {
     private String nome;
 
