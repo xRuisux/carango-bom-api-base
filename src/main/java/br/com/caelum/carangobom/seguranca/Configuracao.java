8a84f447cee701a6ff3ed022185599bb3a4a8adc
Natália Gonçalves Machado <natalia.machado@MacBook-Pro-de-Natalia.local>
2021-08-19 18:51:56 -0300

    (feat) add cors config

--- src/main/java/br/com/caelum/carangobom/seguranca/Configuracao.java
+++ src/main/java/br/com/caelum/carangobom/seguranca/Configuracao.java
@@ -1,5 +1,7 @@
 package br.com.caelum.carangobom.seguranca;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -7,12 +9,12 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
 
 import br.com.caelum.carangobom.usuario.UsuarioRepository;
 
@@ -43,7 +45,14 @@ public class Configuracao extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().antMatchers(HttpMethod.GET, "/veiculos").permitAll()
+        http.cors().configurationSource(request -> {
+            var cors = new CorsConfiguration();
+            cors.setAllowedOrigins(List.of("http://localhost:3000"));
+            cors.setAllowedMethods(List.of("GET","POST", "PUT", "DELETE","PATCH", "OPTIONS"));
+            cors.setAllowedHeaders(List.of("*"));
+            return cors;
+          }).and()
+        .authorizeRequests().antMatchers(HttpMethod.GET, "/veiculos").permitAll()
                 .antMatchers(HttpMethod.GET, "/veiculos/*").permitAll().antMatchers(HttpMethod.POST, "/autenticacao")
                 .permitAll().anyRequest().authenticated().and().csrf().disable().sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
