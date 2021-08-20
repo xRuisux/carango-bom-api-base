d030e504d924d53ccf286e2c2ec2784d70a19c60
Natália Gonçalves Machado <natalia.machado@MacBook-Pro-de-Natalia.local>
2021-08-20 10:31:31 -0300

    (test) autenticao testes

--- src/main/java/br/com/caelum/carangobom/autenticacao/AutenticacaoForm.java
+++ src/main/java/br/com/caelum/carangobom/autenticacao/AutenticacaoForm.java
@@ -5,10 +5,12 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
+@Setter
 public class AutenticacaoForm {
     private String email;
     private String senha;
@@ -16,4 +18,5 @@ public class AutenticacaoForm {
     public UsernamePasswordAuthenticationToken converter() {
         return new UsernamePasswordAuthenticationToken(email, senha);
     }
+
 }
