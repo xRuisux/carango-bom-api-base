c62336cf76d0158d6e3c21e22a67523d04421a42
Natália Gonçalves Machado <natalia.machado@MacBook-Pro-de-Natalia.local>
2021-08-24 10:43:03 -0300

    fix sintaxe

--- src/test/java/br/com/caelum/carangobom/seguranca/TokenServiceTest.java
+++ src/test/java/br/com/caelum/carangobom/seguranca/TokenServiceTest.java
@@ -7,7 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 
-@/*SpringBootTest
+/*@SpringBootTest
 @TestPropertySource(properties = {"carangobom.jwt.secret: my-secret"})*/
 public class TokenServiceTest {
     /*@Autowired
