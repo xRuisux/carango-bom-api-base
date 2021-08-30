ae4be142c686643487c20fc0a482bcc1f41b2058
jessicasimplicio <jessicasimplicio1@gmail.com>
2021-08-30 17:48:35 -0300

    fix: add ResponseStatusException in vehicle service

--- src/test/java/br/com/caelum/carangobom/vehicle/VehicleControllerTest.java
+++ src/test/java/br/com/caelum/carangobom/vehicle/VehicleControllerTest.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.web.util.UriComponentsBuilder;
+import org.springframework.web.server.ResponseStatusException;
 
 import br.com.caelum.carangobom.brand.Brand;
 import javassist.NotFoundException;
@@ -84,7 +85,7 @@ class VehicleControllerTest {
         Vehicle vehicle = new Vehicle(800000, 2020, "CR-V", brand);
         VehicleForm form = new VehicleForm(vehicle);
 
-        assertThrows(NotFoundException.class, () -> {
+        assertThrows(ResponseStatusException.class, () -> {
             vehicleController.update(2L, form);
         });
     }
